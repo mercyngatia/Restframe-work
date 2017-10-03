15da2d9cce58c683263be82ccbf60cd3013082cb
mercyngatia <mercyngatia02@gmail.com>
2017-10-03 03:48:31 +0000

    django setup files

--- my_courses/settings.py
+++ my_courses/settings.py
@@ -122,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+
